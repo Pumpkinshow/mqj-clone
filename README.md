6053fdd2734b81280192fc61d12ab2dde8c1ec56
pumpkin <Pumpkin.Lian@handeson.com>
2020-06-11 11:38:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# mqj-clone
+
+> mqj-clone 克隆仓库到指定文件夹
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install mqj-clone -g
+
+# use
+mqj-clone [github/gitlab/Bitbucket]:[账户名]/[仓库名] [存放地址]
+
+#example
+mqj-clone github:jsyczhanghao/monitorjs ./dist
---
+++ bin/index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+const program = require('commander');
+const download = require("download-git-repo");
+// const exists = require('fs').existsSync;
+// const path = require('path');
+const chalk = require('chalk');
+
+
+
+program
+    .version('1.0.1', '-v, --version')
+    .usage('<origin> [dist]')
+    // .usage('<template-name> [project-name]')
+    // .option('-o, --origin', 'use git clone')
+    // .option('-d, --dist', 'use destination address')
+    // .option('--offline', 'use cached template')
+
+/**
+* Help.
+*/
+
+program.on('--help', () => {
+    console.log('  Examples: ')
+    console.log()
+    console.log(chalk.gray('    # mqj-clone [github/gitlab/Bitbucket]:[账户名]/[仓库名]'))
+    console.log('    $ mqj-clone github:vuejs/vue-cli')
+    console.log(chalk.gray('    # mqj-clone [github/gitlab/Bitbucket]:[账户名]/[仓库名] [存放地址]'))
+    console.log('    $ mqj-clone github:vuejs/vue-cli ./dist')
+    console.log()
+});
+
+function help () {
+    program.parse(process.argv)
+    // console.log(program)
+    if (program.args.length < 1) {
+        return program.help()
+    } else {
+        console.log('params: ' + JSON.stringify(program.args));
+        const dist = program.args[1] ? program.args[1] : './';
+        download(program.args[0], dist, function (err) {
+            console.log(err ? chalk.red(err) : chalk.green('success'));
+        })
+    }
+}
+help()
+
